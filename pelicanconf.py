461d956aad8184faf34cbffc052bcaf0a7e79248
Joseph Courtney <joseph.m.courtney@gmail.com>
2023-03-20 10:42:37 -0400

    repair

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,53 +1,52 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
-AUTHOR = "Joseph Courtney"
-SITENAME = "Joseph Courtney"
-SITEURL = ""
-TIMEZONE = "America/New_York"
-AVATAR = "/images/me.png"
-SIDEBAR_DIGEST = "Physical Chemist"
-SIDEBAR_DIGEST = "Physical Chemist"
-FAVICOO = "/extra/favicon.ico"
+AUTHOR = 'Joseph Courtney'
+SITENAME = 'Joseph Courtney'
+SITEURL = 'https://josephcourtney.com'
+TIMEZONE = 'US/Eastern'
+AVATAR = '/images/me.png'
+SIDEBAR_DIGEST = 'Physical Chemist'
+FAVICOO = '/extra/favicon.ico'
 DISPLAY_PAGES_ON_MENU = True
-MENUITEMS = (("Blog", SITEURL),)
+MENUITEMS = (('Blog', SITEURL),)
 
-DEFAULT_PAGINATION = 10
 DEFAULT_DATE = (1989, 5, 15, 22, 0, 0)
 
-PATH = "content"
+PATH = 'content'
 
 
-DEFAULT_LANG = "en"
-
-STATIC_PATHS = ["images", "extra"]
-EXTRA_PATH_METADATA = {
-    "extra/custom.css": {"path": "custom.css"},
-    "extra/robots.txt": {"path": "robots.txt"},
-    "extra/favicon.ico": {"path": "favicon.ico"},
-    "extra/CNAME": {"path": "CNAME"},
-    "extra/LICENSE": {"path": "LICENSE"},
-    "extra/README": {"path": "README"},
-}
-
-ARTICLE_PATHS = ["articles"]
-PAGE_PATHS = ["pages"]
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ATOM = "feeds/atom.xml"
-AUTHOR_FEED_ATOM = "Joseph Courtney"
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+STATIC_PATHS = ['images', 'extra']
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'custom.css'},
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/CNAME': {'path': 'CNAME'},
+    'extra/LICENSE': {'path': 'LICENSE'},
+    'extra/README': {'path': 'README'},
+}
+
+ARTICLE_PATHS = ['articles']
+PAGE_PATHS = ['pages']
+
 
 # Social widget
 SOCIAL = (
-    ("twitter", "http://twitter.com/JosephMCourtney"),
-    ("github", "http://github.com/josephcourtney"),
-    ("google_scholar", "https://scholar.google.com/citations?user=z3J37tYAAAAJ&hl=en"),
-    ("orcid", "https://orcid.org/0000-0003-1455-4349"),
+    ('twitter', 'http://twitter.com/JosephMCourtney'),
+    ('github', 'http://github.com/josephcourtney'),
+    ('google_scholar', 'https://scholar.google.com/citations?user=z3J37tYAAAAJ&hl=en'),
+    ('orcid', 'https://orcid.org/0000-0003-1455-4349'),
 )
 
+DEFAULT_PAGINATION = 10
 DISPLAY_SUMMARY = True
 
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+#RELATIVE_URLS = True
